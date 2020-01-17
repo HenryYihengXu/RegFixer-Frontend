2bbebfe28d206a0e08ab9740ca47fcca83e44dea
HenryYihengXu <xu443@wisc.edu>
2020-01-16 20:52:16 -0600

    changed backend command, changed application name, icon

--- RegexFixer-backend/index.js
+++ RegexFixer-backend/index.js
@@ -65,25 +65,23 @@ app
       }
     }
     //res.send("request received");
-    const { stdout, stderr } = await exec("java Add 1 2");
-    res.send(stdout);
+    //const { stdout, stderr } = await exec("java Add 1 2");
+    //res.send(stdout);
 
-    // fs.writeFile(fileName, input, async () => {
-    //   // const { stdout, stderr } = await exec("java Add 1 2"); // TODO: change this command to the correct one
-    //   // const { stdout, stderr } = await exec("java -jar target/regfixer.jar -m 1 fix --file tests/benchmark_explicit/test_ssn.txt");
-    //   const { stdout, stderr } = await exec(
-    //     "java -Djava.library.path=:./Z3-4.4.1-X64-OSX-10.11 -jar target/regfixer.jar -m 1 fix --file " +
-    //       fileName
-    //   );
-    //   console.log("stdout:", stdout);
-    //   console.log("stderr:", stderr);
-    //   let result = stdout.match("#sol#.*#sol#");
-    //   if (result === "") {
-    //     result = "Solution not found, try to revise your regex.";
-    //   }
-    //   res.send(result);
-    // });
-    //res.send("result");
+    fs.writeFile(fileName, input, async () => {
+      const { stdout, stderr } = await exec(
+        "java -Djava.library.path=:./ -jar target/regfixer.jar -m 1 fix --file " +
+          fileName
+      );
+      console.log("stdout:", stdout);
+      console.log("stderr:", stderr);
+      let result = stdout.match("#sol#.*#sol#");
+      if (result === "") {
+        result = "Solution not found, try to revise your regex.";
+      }
+      res.send(result);
+    });
+    res.send("result");
   })
   .listen(port, () => {
     console.log(`listening on port ${port}`);
